fa0a13ae425fbb5f97e6aadbcb9172c4b58e9718
gando537 <gandohd@gmail.com>
2024-04-05 15:22:25 +0200

    first commit

---
+++ src/main/java/com/example/gameresearch/GameResearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameResearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gameresearch/GameResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
